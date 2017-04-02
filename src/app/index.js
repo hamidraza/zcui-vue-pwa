8c293973450f6f0d5c4b10142dbff98930daaf27
Hamid Raza <contact@hamidraza.net>
2017-04-03 04:07:48 +0530

    added vue-head and used in app

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,15 @@
 /* @flow */
 
-import Vue  from 'vue';
-import Vuex from 'vuex';
+import Vue     from 'vue';
+import Vuex    from 'vuex';
+import VueHead from 'vue-head';
 
-import App   from './app.vue';
-import store from '~/store';
+import App    from './app.vue';
+import store  from '~/store';
 import router from '~/routes';
 
 Vue.use(Vuex);
+Vue.use(VueHead);
 
 new Vue({
   router,
